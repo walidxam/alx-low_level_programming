4145b277ffabc60dc6cdf575e621b515a27bc8ff
walidxam <93daudi@gmail.com>
2023-03-22 12:09:08 +0100

    new

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * add - Something
+ *
+ * @a: j
+ * @b: f
+ * Description: 'print_alphabet is a function'
+ *
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+
+}
